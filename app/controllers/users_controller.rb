6ab4179a2b8afa58d715d6da3f90d23219981e07
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-03 11:27:31 +0000

    応用課題9b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
-    @user=User.find(params[:id])
-    @books = @user.books.page(params[:page]).reverse_order
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
-    @book=Book.new
   end
 
   def index
@@ -25,6 +25,18 @@ class UsersController < ApplicationController
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
       render "edit"
+    end
+  end
+
+  def search
+    @user = User.find(params[:user_id])
+    @books = @user.books
+    @book = Book.new
+    if params[:created_at] == ""
+      @search_book = "日付を選択してください"#①
+    else
+      create_at = params[:created_at]
+      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count#②
     end
   end
 

98761fe4f9d198eb790296e7bccdb0f09075054a
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-03 10:50:25 +0000

    応用課題8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -36,4 +36,13 @@ class Book < ApplicationRecord
   scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
   scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) } # 6日前
 
+
+　scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_2days, -> { where(created_at: 2.days.ago.all_day) } 
+  scope :created_3days, -> { where(created_at: 3.days.ago.all_day) } 
+  scope :created_4days, -> { where(created_at: 4.days.ago.all_day) } 
+  scope :created_5days, -> { where(created_at: 5.days.ago.all_day) } 
+  scope :created_6days, -> { where(created_at: 6.days.ago.all_day) } 
+
 end
